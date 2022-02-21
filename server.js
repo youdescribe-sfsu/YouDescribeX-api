ce1cbde80ab91ace7d901db3c6d12252aee7e824
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-02-21 15:12:22 -0800

    added user route

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+//routes
+const users = require('./routes/users');
+
 //database
 const db = require('./db');
+
 // Test DB connection
 db.authenticate()
   .then(() => console.log('Connected to YDXAI Database'))
@@ -11,6 +15,11 @@ db.authenticate()
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => res.send('INDEX'));
+
+//User Routes
+app.use('./user', users);
 
 port = 4000;
 app.listen(port, () => console.log(`YDX Backend server on port ${port}!`));
