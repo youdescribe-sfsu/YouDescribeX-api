f45cfdf2e6da8e09cf8b5003247ee5f442fd5337
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-02-26 12:10:45 -0800

    all require statements

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 //routes
 const users = require('./routes/users');
+const videos = require('./routes/videos');
+const dialog_timestamps = require('./routes/dialog_timestamps');
+const audio_descriptions = require('./routes/audio_descriptions');
+const notes = require('./routes/notes');
+const audio_clips = require('./routes/audio_clips');
+
 //database
 const db = require('./db.js');
 
@@ -15,6 +21,15 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('INDEX'));
+
+db.sync({ force: true })
+  // db.sync()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 port = 4000;
 //User Routes
