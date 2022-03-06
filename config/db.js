c6148eba658c846de279410a3e7ca9b198418857
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-03-05 19:37:49 -0800

    removed db code to a separate file

--- config/db.js
+++ config/db.js
@@ -5,4 +5,19 @@ const db = new Sequelize('YDXAI', 'postgres', 'password', {
   dialect: 'postgres',
 });
 
+// Test DB connection
+db.authenticate()
+  .then(() => console.log('Connected to YDXAI Database'))
+  .then(() => {
+    // db.sync({ alter: true })
+    db.sync()
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => console.log('Error' + err));
+
 module.exports = db;
--- server.js
+++ server.js
@@ -7,27 +7,9 @@ const dialog_timestamps = require('./routes/dialog_timestamps');
 const audio_descriptions = require('./routes/audio_descriptions');
 const notes = require('./routes/notes');
 const audio_clips = require('./routes/audio_clips');
-
-//database
-const db = require('./config/db.js');
-
-// Test DB connection
-db.authenticate()
-  .then(() => console.log('Connected to YDXAI Database'))
-  .catch((err) => console.log('Error' + err));
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-db.sync({ force: true })
-  // db.sync()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
 port = 4000;
 
