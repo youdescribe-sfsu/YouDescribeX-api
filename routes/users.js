98b504ef29e0d7e66fae110853361315efc2f849
Bhavani Goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-03-30 18:34:38 -0700

    added controllers for dialogTimestamps, user, video routes

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
-const db = require('../config/db.js');
-const User = require('../models/User');
-
+const userController = require('../controllers/userController');
 router.use(express.json());
-router.get('/all-users', (req, res) =>
-  User.findAll()
-    .then((users) => {
-      console.log(users);
-      res.sendStatus(200);
-    })
-    .catch((err) => console.log(err))
-);
+
+// Routes - send request to controller where db processing is done
+// get all users route
+router.get('/all-users', userController.getAllUsers);
+// get one user route
+router.get('/get-user/:id', userController.getUser);
 
 module.exports = router;
