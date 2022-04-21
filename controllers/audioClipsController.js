805e0650610c2ea11866815c566caae394bcf4f8
bhavani-goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-04-20 18:49:32 -0700

    PUT routes

--- controllers/audioClipsController.js
+++ controllers/audioClipsController.js
@@ -33,6 +33,7 @@ exports.getADAudioClips = async (req, res) => {
     where: {
       AudioDescriptionAdId: req.params.adId,
     },
+    order: ['clip_sequence_num'],
   })
     .then((ADAudioClips) => {
       console.log(ADAudioClips);
@@ -40,5 +41,106 @@ exports.getADAudioClips = async (req, res) => {
     })
     .catch((err) => {
       console.log(err);
+    });
+};
+
+// PUT Requests
+// update audio clip Title - based on clip_id
+// check if there exists a record, if yes update it
+exports.updateAudioClipTitle = async (req, res) => {
+  Audio_Clips.findOne({
+    where: {
+      clip_id: req.params.clipId,
+    },
+  })
+    .then((obj) => {
+      if (obj) {
+        obj
+          .update({ clip_title: req.body.adTitle || clip.clip_title })
+          .then((clip) => {
+            console.log(clip);
+            return res.send(clip);
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.send(err);
+          });
+      } else {
+        return res.status(404).send({
+          message: 'Audio Clip Not Found',
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.send(err);
+    });
+};
+
+// update audio clip Playback Type - from inline to extended or vice versa - based on clip_id
+// check if there exists a record, if yes update it
+exports.updateAudioClipPlaybackType = async (req, res) => {
+  Audio_Clips.findOne({
+    where: {
+      clip_id: req.params.clipId,
+    },
+  })
+    .then((obj) => {
+      if (obj) {
+        obj
+          .update({
+            playback_type: req.body.clipPlaybackType || clip.playback_type,
+          })
+          .then((clip) => {
+            console.log(clip);
+            return res.send(clip);
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.send(err);
+          });
+      } else {
+        return res.status(404).send({
+          message: 'Audio Clip Not Found',
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.send(err);
+    });
+};
+
+// update audio clip Start Time based on clip_id
+// check if there exists a record, if yes update it
+exports.updateAudioClipStartTime = async (req, res) => {
+  Audio_Clips.findOne({
+    where: {
+      clip_id: req.params.clipId,
+    },
+  })
+    .then((obj) => {
+      if (obj) {
+        obj
+          .update({
+            clip_start_time: req.body.clipStartTime || clip.clip_start_time,
+          })
+          .then((clip) => {
+            console.log(clip);
+            return res.send(clip);
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.send(err);
+          });
+      } else {
+        return res.status(404).send({
+          message: 'Audio Clip Not Found',
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.send(err);
     });
 };
