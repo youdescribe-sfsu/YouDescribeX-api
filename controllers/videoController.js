4e9741bd34f0110b5caf37dfb60db7522a79852a
bhavani-goruganthu <bhavani.goruganthu@mail.sfsu.edu>
2022-04-20 18:47:47 -0700

    added getVideobyYoutubeId controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -27,3 +27,19 @@ exports.getVideo = async (req, res) => {
       console.log(err);
     });
 };
+
+// find one Video by YouTubeVideoId
+exports.getVideobyYoutubeId = async (req, res) => {
+  Video.findOne({
+    where: {
+      youtube_video_id: req.params.youtubeId,
+    },
+  })
+    .then((videos) => {
+      console.log(videos);
+      return res.send(videos);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
