afd60ded0f52cce2fa41efe38b34df9520764fdd
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-05-06 18:42:45 -0700

    added new api for generating mp3 for all description texts in the database

--- controllers/audioClipsController.js
+++ controllers/audioClipsController.js
@@ -1,5 +1,138 @@
 const Audio_Clips = require('../models/Audio_Clips');
+const Audio_Descriptions = require('../models/Audio_Descriptions');
+const Videos = require('../models/Videos');
 const generateMp3forDescriptionText = require('../processor/textToSpeech');
+
+// db processing to generate mp3 for all audio clip texts
+exports.generateMP3ForAllClipsInDB = async (req, res) => {
+  // generate mp3 for all audio clips - given audio description ID as a parameter
+  Audio_Clips.findAll({
+    where: {
+      AudioDescriptionAdId: req.params.adId,
+    },
+    // nesting the associations to fetch userID, videoID, YoutubeVideoID
+    include: [
+      {
+        model: Audio_Descriptions,
+        attributes: ['UserUserId', 'VideoVideoId'],
+        include: [
+          {
+            model: Videos,
+            attributes: ['youtube_video_id'],
+          },
+        ],
+      },
+    ],
+  })
+    // get all the AudioClips Data along with userID, videoID, YoutubeVideoID
+    .then((ADAudioClips) => {
+      // proceed only if audioclips exist
+      if (ADAudioClips.length > 0) {
+        // initialize an array to store the description texts
+        let descriptionTexts = [];
+        ADAudioClips.forEach((clip) => {
+          // create an obj to store the related stuff
+          let obj = {
+            clip_id: clip.clip_id,
+            clip_description_type: clip.description_type,
+            clip_description_text: clip.description_text,
+            video_id: clip.Audio_Description.VideoVideoId,
+            user_id: clip.Audio_Description.UserUserId,
+            youtube_id: clip.Audio_Description.Video.youtube_video_id,
+          };
+          // push each clip data to the array
+          descriptionTexts.push(obj);
+        });
+        return descriptionTexts;
+      }
+      // if there are no matching audioclips
+      else {
+        return res.status(404).send({
+          message: 'No Audio Clips found!! Please try again',
+        });
+      }
+    })
+    .then(async (descriptionTexts) => {
+      // initialize an empty array for storing statusData
+      let statusData = [];
+      // wait for text to speech generation of all description texts
+      await Promise.all(
+        descriptionTexts.map(async (desc) => {
+          // add the text to speech output and the clip_id to an object and push to statusData
+          let data = {
+            textToSpeechOutput: await generateMp3forDescriptionText(
+              desc.user_id,
+              desc.youtube_id,
+              desc.clip_description_text,
+              desc.clip_description_type
+            ),
+            clip_id: desc.clip_id,
+          };
+          statusData.push(data);
+        })
+        // status Data now has text to speech generation status of each clip_id
+      ).then(async () => {
+        let updateStatusOfAllClips = [];
+        // wait until all promises are done
+        await Promise.all(
+          statusData.map(async (data) => {
+            // to update the clip_audio_path column of Audio_Clips Table with the generated audio path
+            let updateStatus = await updateClipAudioPathInDB(data);
+            // get the status message and push it to an array
+            updateStatusOfAllClips.push(updateStatus);
+          })
+        ).then(() => {
+          // send the status messages along with the clip_id's as a response
+          res.send(updateStatusOfAllClips);
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send({
+        message: 'Unable to connect to DB!! Please try again',
+      }); // send error message
+    });
+};
+
+// to update the clip_audio_path column of Audio_Clips Table with the generated audio path
+async function updateClipAudioPathInDB(data) {
+  // check if there is an error in text to speech generation
+  if (!data.textToSpeechOutput.status) {
+    let updateStatus = {
+      clip_id: data.clip_id,
+      message: 'Unable to generate Text to Speech!! Please try again',
+    };
+    return updateStatus;
+  }
+  // text to speech generation is successful
+  else {
+    // update the path of the audio file & the description text for the audio clip in the db
+    let updateStatus = Audio_Clips.update(
+      { clip_audio_path: data.textToSpeechOutput.filepath },
+      {
+        where: {
+          clip_id: data.clip_id,
+        },
+      }
+    )
+      .then(() => {
+        let status = { clip_id: data.clip_id, message: 'Success OK' };
+        return status;
+        // return the success msg with the clip_id
+      })
+      .catch((err) => {
+        console.log(err);
+        let status = {
+          clip_id: data.clip_id,
+          message: 'Unable to Update Audio Path in the DB!! Please try again',
+        };
+        return status;
+        // return the error msg with the clip_id
+      });
+    return updateStatus;
+  }
+}
 
 // db processing is done here using sequelize models
 // find all Audio_Clips
@@ -35,6 +168,7 @@ exports.getADAudioClips = async (req, res) => {
       AudioDescriptionAdId: req.params.adId,
     },
     order: ['clip_start_time'],
+    logging: false,
   })
     .then((ADAudioClips) => {
       console.log(ADAudioClips);
@@ -175,7 +309,6 @@ exports.updateAudioDescription = async (req, res) => {
                 req.body.clipDescriptionText || clip.description_text,
             })
             .then((clip) => {
-              console.log('status ok');
               return res.status(200).send({
                 message: 'Success OK',
               });
@@ -193,7 +326,7 @@ exports.updateAudioDescription = async (req, res) => {
         }
       })
       .catch((err) => {
-        console.log(err);
+        // console.log(err);
         return res.status(500).send({
           message: 'Unable to connect to DB!! Please try again',
         }); // send error message
