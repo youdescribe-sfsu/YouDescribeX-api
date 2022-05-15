a87defd9f386a52521bbe57346b9f829315a185b
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-05-14 21:12:50 -0700

    removed unused routes & updated post route

--- routes/notes.js
+++ routes/notes.js
@@ -5,14 +5,8 @@ const notesController = require('../controllers/notesController');
 router.use(express.json());
 
 // Routes - send request to controller where db processing is done
-// get all Notes route
-router.get('/all-Notes', notesController.getAllNotes);
-// get one Note route - based on notes_id
-router.get('/get-note-byId/:id', notesController.getNoteById);
-// get one Note route - based on AdId
-router.get('/get-note-byAdId/:adId', notesController.getNoteByAdId);
 
-// Post Routes
+// Post Routes - // create / update note
 router.post('/post-note', notesController.postNoteByAdId);
 
 module.exports = router;
