008e7d1b2b2022ac755b48ce232e971c50d71a42
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-05-18 19:59:52 -0700

    updating playback type  & error handling

--- audioClipHelperFunctions/updateClipDataInDB.js
+++ audioClipHelperFunctions/updateClipDataInDB.js
@@ -1,6 +1,7 @@
 const Audio_Clips = require('../models/Audio_Clips');
 // import helper files
 const calculateClipEndTime = require('./calculateClipEndTime');
+const analyzePlaybackType = require('./analyzePlaybackType');
 // import processor files
 const getAudioDuration = require('../processors/getAudioDuration');
 
@@ -16,40 +17,88 @@ const updateDataInDB = async (data) => {
   }
   // text to speech generation is successful
   else {
+    let updateStatus;
+
     // calculate audio duration
     console.log('Generating Audio Duration');
-    let clipDuration = await getAudioDuration(data.textToSpeechOutput.filepath);
-    // calculate audio clip end time
-    console.log('Generating Audio Clip End Time');
-    let clipEndTime = await calculateClipEndTime(data.clip_id, clipDuration);
-    // update the path of the audio file & the description text for the audio clip in the db
-    let updateStatus = await Audio_Clips.update(
-      {
-        clip_audio_path: data.textToSpeechOutput.filepath,
-        clip_duration: parseFloat(clipDuration),
-        clip_end_time: parseFloat(clipEndTime),
-      },
-      {
-        where: {
-          clip_id: data.clip_id,
-        },
-      }
-    )
-      .then(() => {
-        let status = { clip_id: data.clip_id, message: 'Success OK' };
-        return status;
-        // return the success msg with the clip_id
-      })
-      .catch((err) => {
-        console.log(err);
-        let status = {
-          clip_id: data.clip_id,
-          message: 'Unable to Update Audio Path in the DB!! Please try again',
-        };
-        return status;
-        // return the error msg with the clip_id
+    let clipDurationStatus = await getAudioDuration(
+      data.textToSpeechOutput.filepath
+    );
+    // check if the returned data is null - an error in generating Audio Duration
+    if (clipDurationStatus.data === null) {
+      return (updateStatus = {
+        clip_id: data.clip_id,
+        message: clipDurationStatus.message,
       });
-    return updateStatus;
+    } else {
+      // Audio Duration generation successful
+      const clipDuration = clipDurationStatus.data;
+
+      // calculate audio clip end time
+      console.log('Generating Audio Clip End Time');
+      let clipEndTimeStatus = await calculateClipEndTime(
+        data.clip_id,
+        clipDuration
+      );
+      // check if the returned data is null - an error in calculating Clip End Time
+      if (clipEndTimeStatus.data === null) {
+        return (updateStatus = {
+          clip_id: data.clip_id,
+          message: clipEndTimeStatus.message,
+        });
+      } else {
+        // Clip End Time Calculation Successful
+        const clipEndTime = clipEndTimeStatus.data;
+
+        // analyze clip playback type from dialog timestamp data
+        console.log('Analyzing PlaybackType Based on Dialog Timestamp Data');
+        let playbackTypeStatus = await analyzePlaybackType(
+          data.clip_id,
+          clipEndTime,
+          data.video_id
+        );
+        // check if the returned data is null - an error in analyzing Playback type
+        if (playbackTypeStatus.data === null) {
+          return (updateStatus = {
+            clip_id: data.clip_id,
+            message: playbackTypeStatus.message,
+          });
+        } else {
+          // Clip Playback Type is returned
+          const playbackType = playbackTypeStatus.data;
+
+          // update the path of the audio file, duration, end time & playback type of the audio clip in the db
+          let updateStatus = await Audio_Clips.update(
+            {
+              clip_audio_path: data.textToSpeechOutput.filepath,
+              clip_duration: parseFloat(clipDuration),
+              clip_end_time: parseFloat(clipEndTime),
+              playback_type: playbackType,
+            },
+            {
+              where: {
+                clip_id: data.clip_id,
+              },
+            }
+          )
+            .then(() => {
+              let status = { clip_id: data.clip_id, message: 'Success OK' };
+              return status;
+              // return the success msg with the clip_id
+            })
+            .catch((err) => {
+              console.log(err);
+              let status = {
+                clip_id: data.clip_id,
+                message: 'Unable to Update DB!! Please try again',
+              };
+              return status;
+              // return the error msg with the clip_id
+            });
+          return updateStatus;
+        }
+      }
+    }
   }
 };
 
--- processors/getAudioDuration.js
+++ processors/getAudioDuration.js
@@ -2,9 +2,19 @@ const { getAudioDurationInSeconds } = require('get-audio-duration');
 
 const getAudioDuration = async (filepath) => {
   const path = filepath.replace('.', './public');
-  return await getAudioDurationInSeconds(path).then((duration) => {
-    return duration;
-  });
+  return await getAudioDurationInSeconds(path)
+    .then((duration) => {
+      return {
+        message: 'Success',
+        data: duration,
+      };
+    })
+    .catch((err) => {
+      return {
+        message: 'Error in Getting Audio Duration!! Please try again',
+        data: null,
+      };
+    });
 };
 
 module.exports = getAudioDuration;
