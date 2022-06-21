e11eb4417c97b1e39ceff479def5b35cb3a5f79f
bhavani-goruganthu <bhavani.goruganthu@gmail.com>
2022-06-21 08:17:24 -0700

    add createuserlinks route

---
+++ userLinksHelperFunctions/checkIfAdExists.js
@@ -0,0 +1,29 @@
+const Audio_Descriptions = require('../models/Audio_Descriptions');
+
+// check if there is an existing Audio Description.. If yes, throw an error
+const checkIfAdExists = async (userId, videoId) => {
+  return Audio_Descriptions.findOne({
+    where: {
+      VideoVideoId: videoId,
+      UserUserId: userId,
+    },
+  })
+    .then((adData) => {
+      if (adData === null) {
+        return { message: 'Success', data: [] };
+      } else {
+        return {
+          message: 'AudioDescription already exists for this user!!',
+          data: null,
+        };
+      }
+    })
+    .catch((err) => {
+      return {
+        message: 'Error Connecting to DB!! Please try again.',
+        data: null,
+      };
+    });
+};
+
+module.exports = checkIfAdExists;
