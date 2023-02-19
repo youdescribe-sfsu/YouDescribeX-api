a265e178cafc84182cc840a9d17dd68402f69627
ManaliSeth <sethmanali@gmail.com>
2023-02-19 09:01:37 -0800

    Added DialogTimestamps Route

---
+++ src/controllers/dialogTimestamps.controller.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { Dialog_TimestampsAttributes } from '../models/postgres/Dialog_Timestamps';
+import DialogTimestampsService from '../services/dialogTimestamps.service';
+import { IDialogTimeStamps } from '../interfaces/dialogTimestamps.interface';
+
+class DialogTimestampsController {
+    public dialogTimestampsService = new DialogTimestampsService();
+
+    public getVideoDialogTimestamps = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+        const videoId: string = req.params.videoId;
+        const videoDialogTimestamps: IDialogTimeStamps | Dialog_TimestampsAttributes = await this.dialogTimestampsService.getVideoDialogTimestamps(videoId);
+        
+        res.status(200).json(videoDialogTimestamps);
+        } catch (error) {
+        next(error);
+        }
+    };
+}  
+export default DialogTimestampsController;
---
+++ src/services/dialogTimestamps.service.ts
@@ -0,0 +1,24 @@
+import { HttpException } from '../exceptions/HttpException';
+import { isEmpty } from '../utils/util';
+import { CURRENT_DATABASE } from '../config';
+import { Dialog_TimestampsAttributes, PostGres_Dialog_Timestamps } from '../models/postgres/init-models';
+import { IDialogTimeStamps } from '../interfaces/dialogTimestamps.interface';
+// Dialog Timestamps Imports
+import { DialogTimeStamps as mongodbDialogTimestamps } from '../models/mongodb/DialogTimeStamps.mongo.model';
+class DialogTimestampsService {
+  public async getVideoDialogTimestamps(videoId: string): Promise<IDialogTimeStamps | Dialog_TimestampsAttributes> {
+    if (isEmpty(videoId)) throw new HttpException(400, 'videoId is empty');
+
+    if (CURRENT_DATABASE == 'mongodb') {
+        const findDialogTimestampsById: IDialogTimeStamps = await mongodbDialogTimestamps.findOne({ VideoVideoId: videoId });
+        if (!findDialogTimestampsById) throw new HttpException(409, "Dialog Timestamp for this YouTube Video doesn't exist");
+        return findDialogTimestampsById;
+      } else {
+        const findDialogTimestampsById: Dialog_TimestampsAttributes = await PostGres_Dialog_Timestamps.findOne({ where: { VideoVideoId: videoId } });
+        if (!findDialogTimestampsById) throw new HttpException(409, "Dialog Timestamp for this YouTube Video doesn't exist");
+        return findDialogTimestampsById;
+      }
+    }
+}
+
+export default DialogTimestampsService;
