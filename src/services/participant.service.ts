aa929cde5890f46ff873edffbd4f18eb24a9295c
vishals9711 <vishalsharma1907@gmail.com>
2023-02-20 21:06:01 -0800

    Added Participants Route

---
+++ src/services/participant.service.ts
@@ -0,0 +1,45 @@
+import { IAddNewParticipant } from '../interfaces/timings.interface';
+import { CURRENT_DATABASE } from '../config';
+import { HttpException } from '../exceptions/HttpException';
+import { Participants, PostGres_Participants } from '../models/postgres/init-models';
+import { isEmpty } from '../utils/util';
+
+class ParticipantService {
+  public async addNewParticipant(newParticpantData: IAddNewParticipant): Promise<Participants> {
+    const { email, name, userIdWithAi, userIdWithoutAi, youtubeVideoIdWithAi, youtubeVideoIdWithoutAi } = newParticpantData;
+
+    if (isEmpty(email)) throw new HttpException(400, 'Email is empty');
+    if (isEmpty(name)) throw new HttpException(400, 'Name is empty');
+    if (isEmpty(userIdWithAi)) throw new HttpException(400, 'userIdWithAi is empty');
+    if (isEmpty(userIdWithoutAi)) throw new HttpException(400, 'userIdWithoutAi is empty');
+    if (isEmpty(youtubeVideoIdWithAi)) throw new HttpException(400, 'youtubeVideoIdWithAi is empty');
+    if (isEmpty(youtubeVideoIdWithoutAi)) throw new HttpException(400, 'youtubeVideoIdWithoutAi is empty');
+
+    if (CURRENT_DATABASE == 'mongodb') {
+    } else {
+      const participantResponse = await PostGres_Participants.create({
+        participant_email: email,
+        participant_name: name,
+        user_id_with_AI: userIdWithAi,
+        user_id_without_AI: userIdWithoutAi,
+        youtube_video_id_with_AI: youtubeVideoIdWithAi,
+        youtube_video_id_without_AI: youtubeVideoIdWithoutAi,
+      });
+      if (!participantResponse) throw new HttpException(409, 'Unable To create new participant');
+      return participantResponse;
+    }
+  }
+
+  public async getParticipantById(participantId: string): Promise<Participants> {
+    if (isEmpty(participantId)) throw new HttpException(400, 'ParticipantId is empty');
+
+    if (CURRENT_DATABASE == 'mongodb') {
+    } else {
+      const participantResponse = await PostGres_Participants.findByPk(participantId);
+      if (!participantResponse) throw new HttpException(409, `Cannot find participant with id=${participantId}`);
+      return participantResponse;
+    }
+  }
+}
+
+export default ParticipantService;
