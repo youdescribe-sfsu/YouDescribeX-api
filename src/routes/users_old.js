6259578308e70251fc91da3707f8bdd0c79bea51
vishals9711 <vishalsharma1907@gmail.com>
2023-02-26 15:24:38 -0800

    Removed JS files

--- src/routes/users_old.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.use(express.json());
-
-// Routes - send request to controller where db processing is done
-
-module.exports = router;
