6920501d67908be1655d2aca6e4e21a8479179ab
vishals9711 <vishalsharma1907@gmail.com>
2023-02-26 15:06:37 -0800

    Added Routes for Audio Clips

--- src/routes/audio_descriptions.route.ts
+++ src/routes/audio_descriptions.route.ts
@@ -3,20 +3,20 @@ import AudioDescriptionsController from '../controllers/audioDescriptions.contro
 import { Routes } from '../interfaces/routes.interface';
 
 class AudioDescriptionsRoute implements Routes {
-    public path = '/audioDescriptions';
-    public router = Router();
-    public audioDescriptionsController = new AudioDescriptionsController();
-  
-    constructor() {
-      this.initializeRoutes();
-    }
-  
-    private initializeRoutes() {
-      this.router.get(`${this.path}/get-user-ad/:videoId&:userId`, this.audioDescriptionsController.getUserAudioDescriptionData);
-      this.router.post(`${this.path}/newaidescription`, this.audioDescriptionsController.newAiDescription);
-    //   this.router.post(`${this.path}/newdescription`, this.audioDescriptionsController.newDescription);
-      this.router.delete(`${this.path}/delete-user-ad-audios/:youtubeVideoId&:userId`, this.audioDescriptionsController.deleteUserADAudios);
-    }
+  public path = '/audioDescriptions';
+  public router = Router();
+  public audioDescriptionsController = new AudioDescriptionsController();
+
+  constructor() {
+    this.initializeRoutes();
   }
-  
-  export default AudioDescriptionsRoute;
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}/get-user-ad/:videoId&:userId`, this.audioDescriptionsController.getUserAudioDescriptionData);
+    this.router.post(`${this.path}/newaidescription`, this.audioDescriptionsController.newAiDescription);
+    //   this.router.post(`${this.path}/newdescription`, this.audioDescriptionsController.newDescription);
+    this.router.delete(`${this.path}/delete-user-ad-audios/:youtubeVideoId&:userId`, this.audioDescriptionsController.deleteUserADAudios);
+  }
+}
+
+export default AudioDescriptionsRoute;
