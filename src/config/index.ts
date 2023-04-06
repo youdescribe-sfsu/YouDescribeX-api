1f2d8cc6d89fb8bb9afa77ed5fc0ef58818dea08
vishals9711 <vishalsharma1907@gmail.com>
2023-04-05 18:28:44 -0700

    Setup read from MongoDB

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,3 +7,4 @@ export const { MONGO_DB_HOST, MONGO_DB_PORT, MONGO_DB_DATABASE } = process.env;
 export const { POSTGRES_DB_NAME, POSTGRES_DB_USER, POSTGRES_DB_PASSWORD, POSTGRES_DB_HOST, POSTGRES_DB_PORT } = process.env;
 export const { CURRENT_DATABASE } = process.env;
 export const { AUDIO_DIRECTORY = '/public/audio' } = process.env;
+export const { CURRENT_MONGO_DB = 'YDX' } = process.env;
