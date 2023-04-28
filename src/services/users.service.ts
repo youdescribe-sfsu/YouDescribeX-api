6f52a9611c665f728a09a0085c9cedb67836bcf7
Benjamin Kao <benjamin.kao00@gmail.com>
2023-04-27 21:24:33 -0700

    Commented out Postgres unimplemented createNewUserAD code

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -274,65 +274,65 @@ class UserService {
       // TODO: Need to update PostgreSQL version to search for AI Audio Description for specified YouTube Video ID
       logger.error('PostgreSQL Version for CreateNewUserAudioDescription() Not Implemented');
       throw new HttpException(500, `Not Implemented Error`);
-      // Check if Video exists
-      const videoIdStatus = await PostGres_Videos.findOne({
-        where: { youtube_video_id: youtubeVideoId },
-      });
-      if (!videoIdStatus) throw new HttpException(409, "Video doesn't exist");
+      // // Check if Video exists
+      // const videoIdStatus = await PostGres_Videos.findOne({
+      //   where: { youtube_video_id: youtubeVideoId },
+      // });
+      // if (!videoIdStatus) throw new HttpException(409, "Video doesn't exist");
 
-      // Check if AUDIO DESCRIPTION already exists
-      const checkIfAudioDescriptionExists = await PostGres_Audio_Descriptions.findOne({
-        where: { VideoVideoId: videoIdStatus.video_id, UserUserId: userId },
-      });
-      if (checkIfAudioDescriptionExists) throw new HttpException(409, 'Audio Description already exists');
+      // // Check if AUDIO DESCRIPTION already exists
+      // const checkIfAudioDescriptionExists = await PostGres_Audio_Descriptions.findOne({
+      //   where: { VideoVideoId: videoIdStatus.video_id, UserUserId: userId },
+      // });
+      // if (checkIfAudioDescriptionExists) throw new HttpException(409, 'Audio Description already exists');
 
-      // Check if AI USER exists
-      const checkIfAIUserExists = await PostGres_Users.findOne({
-        where: { user_id: aiUserId },
-      });
-      if (!checkIfAIUserExists) throw new HttpException(409, "AI User doesn't exist");
+      // // Check if AI USER exists
+      // const checkIfAIUserExists = await PostGres_Users.findOne({
+      //   where: { user_id: aiUserId },
+      // });
+      // if (!checkIfAIUserExists) throw new HttpException(409, "AI User doesn't exist");
 
-      // Check if AI Descriptions exists
-      const checkIfAIDescriptionsExists = await PostGres_Audio_Descriptions.findOne({
-        where: { VideoVideoId: videoIdStatus.video_id, UserUserId: aiUserId },
-        include: [
-          {
-            model: PostGres_Audio_Clips,
-            separate: true,
-            order: ['clip_start_time'],
-            as: 'Audio_Clips',
-          },
-        ],
-      });
-      if (!checkIfAIDescriptionsExists) throw new HttpException(409, "AI Descriptions doesn't exist");
-      // Create new Audio Description
-      const createNewAudioDescription = await PostGres_Audio_Descriptions.create({
-        VideoVideoId: videoIdStatus.video_id,
-        UserUserId: userId,
-        is_published: false,
-      });
-      if (!createNewAudioDescription) throw new HttpException(409, "Audio Description couldn't be created");
-      // Create new Audio Clips
-      const createNewAudioClips = await PostGres_Audio_Clips.bulkCreate(
-        checkIfAIDescriptionsExists.Audio_Clips.map(clip => {
-          return {
-            clip_title: clip.clip_title,
-            description_type: clip.description_type,
-            description_text: clip.description_text,
-            playback_type: clip.playback_type,
-            clip_start_time: clip.clip_start_time,
-            is_recorded: false,
-          };
-        }),
-      );
-      if (!createNewAudioClips) throw new HttpException(409, "Audio Clips couldn't be created");
-      createNewAudioClips.forEach(async clip => {
-        createNewAudioDescription.addAudio_Clip(clip);
-      });
-      return {
-        audioDescriptionId: createNewAudioDescription.ad_id,
-        fromAI: true,
-      };
+      // // Check if AI Descriptions exists
+      // const checkIfAIDescriptionsExists = await PostGres_Audio_Descriptions.findOne({
+      //   where: { VideoVideoId: videoIdStatus.video_id, UserUserId: aiUserId },
+      //   include: [
+      //     {
+      //       model: PostGres_Audio_Clips,
+      //       separate: true,
+      //       order: ['clip_start_time'],
+      //       as: 'Audio_Clips',
+      //     },
+      //   ],
+      // });
+      // if (!checkIfAIDescriptionsExists) throw new HttpException(409, "AI Descriptions doesn't exist");
+      // // Create new Audio Description
+      // const createNewAudioDescription = await PostGres_Audio_Descriptions.create({
+      //   VideoVideoId: videoIdStatus.video_id,
+      //   UserUserId: userId,
+      //   is_published: false,
+      // });
+      // if (!createNewAudioDescription) throw new HttpException(409, "Audio Description couldn't be created");
+      // // Create new Audio Clips
+      // const createNewAudioClips = await PostGres_Audio_Clips.bulkCreate(
+      //   checkIfAIDescriptionsExists.Audio_Clips.map(clip => {
+      //     return {
+      //       clip_title: clip.clip_title,
+      //       description_type: clip.description_type,
+      //       description_text: clip.description_text,
+      //       playback_type: clip.playback_type,
+      //       clip_start_time: clip.clip_start_time,
+      //       is_recorded: false,
+      //     };
+      //   }),
+      // );
+      // if (!createNewAudioClips) throw new HttpException(409, "Audio Clips couldn't be created");
+      // createNewAudioClips.forEach(async clip => {
+      //   createNewAudioDescription.addAudio_Clip(clip);
+      // });
+      // return {
+      //   audioDescriptionId: createNewAudioDescription.ad_id,
+      //   fromAI: true,
+      // };
     }
   }
 }
