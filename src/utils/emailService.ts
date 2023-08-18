dd76fd9c652538d4facb607936868aafbcd6704a
SFState <vishalsharma1907@gmail.com>
2023-08-17 19:10:22 -0700

    Fixed email service

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -1,16 +1,18 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+import axios from 'axios';
 import { GMAIL_PASSWORD, GMAIL_USER } from '../config';
+import { logger } from './logger';
 
 async function sendEmail(email: string, subject: string, text: string) {
   try {
     // Create a transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail', // Use your email service provider here if not using Gmail
-      auth: {
-        user: GMAIL_USER, // Your email address
-        pass: GMAIL_PASSWORD, // Your email password or app password
-      },
-    });
+    // const transporter = nodemailer.createTransport({
+    //   service: 'Gmail', // Use your email service provider here if not using Gmail
+    //   auth: {
+    //     user: GMAIL_USER, // Your email address
+    //     pass: GMAIL_PASSWORD, // Your email password or app password
+    //   },
+    // });
 
     // Configure the email details
     const mailOptions = {
@@ -21,10 +23,23 @@ async function sendEmail(email: string, subject: string, text: string) {
     };
 
     // Send the email
-    const info = await transporter.sendMail(mailOptions);
+    // const info = await transporter.sendMail(mailOptions);
 
-    console.log('Email sent:', info.response);
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://eoze3kvt7ugp3rh.m.pipedream.net',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: mailOptions,
+    };
+
+    const info = await axios.request(config);
+    console.log(`Email sent :: ${email}`);
+    logger.info(`Email send :: ${email}`);
   } catch (error) {
+    logger.error(`Error Sending Email Due to Error :: ${error} `);
     console.error('Error sending email:', error);
   }
 }
