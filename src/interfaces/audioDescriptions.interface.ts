d6a9789e5fa0cd13121e0cab87f8919cca57ef94
SFState <vishalsharma1907@gmail.com>
2023-10-14 04:07:04 -0700

    Add format script to package.json and update audio description status handling

--- src/interfaces/audioDescriptions.interface.ts
+++ src/interfaces/audioDescriptions.interface.ts
@@ -2,7 +2,6 @@ import { ObjectId } from 'mongoose';
 
 export interface IAudioDescriptions {
   ad_id: ObjectId;
-  is_published: boolean;
   userUserId: ObjectId | null;
   videoVideoId: ObjectId | null;
 }
