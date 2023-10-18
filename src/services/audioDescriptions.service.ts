662f1ecc46bc3484b91684f95297b2d1c55035a9
Sanjay Mirani <miranisanjay417@gmail.com>
2023-10-18 12:05:12 -0700

    adding pagination for recent descriptions

--- src/services/audioDescriptions.service.ts
+++ src/services/audioDescriptions.service.ts
@@ -432,8 +432,11 @@ class AudioDescriptionsService {
     return newObj;
   };
 
-  public async getRecentDescriptions() {
-    const recentAudioDescriptions = await MongoAudio_Descriptions_Model.find().sort({ created_at: -1 }).limit(10);
+  public async getRecentDescriptions(pageNumber: string) {
+    const page = parseInt(pageNumber, 10);
+    const perPage = 6;
+    const skipCount = (page - 1) * perPage;
+    const recentAudioDescriptions = await MongoAudio_Descriptions_Model.find().sort({ created_at: -1 }).skip(skipCount).limit(perPage);
     const videoIds = recentAudioDescriptions.map(description => description.video);
     const videos = await MongoVideosModel.find({ _id: { $in: videoIds } });
 
