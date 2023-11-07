fe5cf6bb0d9ff90f7e76f2b829f391309e7eb288
Vishal Sharma <vishalsharma1907@gmail.com>
2023-11-07 10:52:18 -0800

    Add cron job to initialize GPU server status check and notification (#30)

---
+++ src/routes/gpu_utils.route.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { Routes } from '../interfaces/routes.interface';
+import GpuUtilsController from '../controllers/gpu_utils.controller';
+
+class AuthRoute implements Routes {
+  public path = '/utils';
+  public router = Router();
+  public gpuUtilsController = new GpuUtilsController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`${this.path}/notify`, this.gpuUtilsController.notify);
+  }
+}
+
+export default AuthRoute;
