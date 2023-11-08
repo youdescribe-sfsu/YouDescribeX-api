f42d5d84fb34985f10c6eca8bf0c18cda0fd3b1a
Sanjay Mirani <60569823+sanjay417@users.noreply.github.com>
2023-11-08 15:46:17 -0800

    initial commit for user-wishlist (#32)

--- src/controllers/wishlist.controller.ts
+++ src/controllers/wishlist.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import WishListService from '../services/wishlist.service';
 import { WishListRequest } from '../dtos/wishlist.dto';
+import { IUser } from '../models/mongodb/User.mongo';
+import { MongoUsersModel } from '../models/mongodb/init-models.mongo';
 
 class WishListController {
   public wishlistService = new WishListService();
@@ -8,9 +10,25 @@ class WishListController {
   public getAllWishlist = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const filterBody: WishListRequest = req.body;
-      console.log('in wishlist controller');
-      console.log(req.user);
       const wishlistResponse = await this.wishlistService.getAllWishlist(filterBody);
+      res.status(201).json(wishlistResponse);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getUserWishlist = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userData = req.user as unknown as IUser;
+      const pageNumber = req.query.pageNumber as string;
+      if (!userData) {
+        throw new Error('User not logged in');
+      }
+      const user = await MongoUsersModel.findById(userData._id);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      const wishlistResponse = await this.wishlistService.getUserWishlist(user._id, pageNumber);
       res.status(201).json(wishlistResponse);
     } catch (error) {
       next(error);
