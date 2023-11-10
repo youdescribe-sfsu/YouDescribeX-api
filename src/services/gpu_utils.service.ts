d2143426f9f61b89873bde62ecebcf6e4ddb8ea7
SFState <vishalsharma1907@gmail.com>
2023-11-10 09:32:25 -0800

    Update email notification text for AI prompted description

--- src/services/gpu_utils.service.ts
+++ src/services/gpu_utils.service.ts
@@ -19,7 +19,7 @@ class GpuUtilsService {
     if (!youtubeVideoData) throw new Error('notifyAiDescriptions :Video not found');
     const previewURL = `${ydx_app_host}/audio-description/preview/${youtube_id}/${audio_description_id}`;
     const subject = `Requested Audio Description for ${youtubeVideoData.title} ready`;
-    const text = `our Audio Description is now available! You're invited to view it by following this link ${previewURL}`;
+    const text = `Your AI prompted description is ready! You're invited to view it by following this link ${previewURL}`;
     for (const user_id of user_ids) {
       const email = await getEmailForUser(user_id);
       await sendEmail(email, subject, text);
