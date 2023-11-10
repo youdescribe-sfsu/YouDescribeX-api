50814c0732286c9eecabe01374654b0963786d01
SFState <vishalsharma1907@gmail.com>
2023-11-09 17:32:12 -0800

    Refactor email to user_id and add getEmailForUser function"Refactor email to user_id and add getEmailForUser function"

--- src/services/gpu_utils.service.ts
+++ src/services/gpu_utils.service.ts
@@ -1,6 +1,7 @@
 import { logger } from '../utils/logger';
 import sendEmail from '../utils/emailService';
 import { getVideoDataByYoutubeId } from './videos.util';
+import { getEmailForUser } from '../utils/util';
 
 class GpuUtilsService {
   public async notify(email: string, subject: string, text: string) {
@@ -13,13 +14,14 @@ class GpuUtilsService {
     }
   }
 
-  public async notifyAiDescriptions(youtube_id: string, audio_description_id: string, ydx_app_host: string, emails: string[]) {
+  public async notifyAiDescriptions(youtube_id: string, audio_description_id: string, ydx_app_host: string, user_ids: string[]) {
     const youtubeVideoData = await getVideoDataByYoutubeId(youtube_id);
     if (!youtubeVideoData) throw new Error('notifyAiDescriptions :Video not found');
     const previewURL = `${ydx_app_host}/audio-description/preview/${youtube_id}/${audio_description_id}`;
     const subject = `Requested Audio Description for ${youtubeVideoData.title} ready`;
     const text = `our Audio Description is now available! You're invited to view it by following this link ${previewURL}`;
-    for (const email of emails) {
+    for (const user_id of user_ids) {
+      const email = await getEmailForUser(user_id);
       await sendEmail(email, subject, text);
     }
   }
