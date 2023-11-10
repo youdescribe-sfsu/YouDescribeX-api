b9ce5418fbc457c07de3e0ae7d40709711bc16e8
SFState <vishalsharma1907@gmail.com>
2023-11-09 17:10:55 -0800

    Add notifyAiDescriptions function to GpuUtilsController and GpuUtilsService

--- src/services/gpu_utils.service.ts
+++ src/services/gpu_utils.service.ts
@@ -1,5 +1,6 @@
 import { logger } from '../utils/logger';
 import sendEmail from '../utils/emailService';
+import { getVideoDataByYoutubeId } from './videos.util';
 
 class GpuUtilsService {
   public async notify(email: string, subject: string, text: string) {
@@ -9,6 +10,17 @@ class GpuUtilsService {
     } catch (error) {
       logger.error(`Error Sending Email Due to Error :: ${JSON.stringify(error)} `);
       throw error;
+    }
+  }
+
+  public async notifyAiDescriptions(youtube_id: string, audio_description_id: string, ydx_app_host: string, emails: string[]) {
+    const youtubeVideoData = await getVideoDataByYoutubeId(youtube_id);
+    if (!youtubeVideoData) throw new Error('notifyAiDescriptions :Video not found');
+    const previewURL = `${ydx_app_host}/audio-description/preview/${youtube_id}/${audio_description_id}`;
+    const subject = `Requested Audio Description for ${youtubeVideoData.title} ready`;
+    const text = `our Audio Description is now available! You're invited to view it by following this link ${previewURL}`;
+    for (const email of emails) {
+      await sendEmail(email, subject, text);
     }
   }
 }
