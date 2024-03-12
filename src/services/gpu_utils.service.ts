9ae338c6c997ee5817f6dcbe0743b2ac379873a0
Sanjay Mirani <miranisanjay417@gmail.com>
2024-03-12 12:11:15 -0700

    sending emails logic rebuild

--- src/services/gpu_utils.service.ts
+++ src/services/gpu_utils.service.ts
@@ -1,7 +1,8 @@
 import { logger } from '../utils/logger';
 import sendEmail from '../utils/emailService';
 import { getEmailForUser } from '../utils/util';
-import { MongoAICaptionRequestModel, MongoUsersModel, MongoVideosModel } from '../models/mongodb/init-models.mongo';
+import { MongoAICaptionRequestModel } from '../models/mongodb/init-models.mongo';
+import { getVideoDataByYoutubeId } from './videos.util';
 
 class GpuUtilsService {
   public async notify(email: string, subject: string, text: string) {
@@ -15,12 +16,13 @@ class GpuUtilsService {
   }
 
   public async notifyAiDescriptions(youtube_id: string, audio_description_id: string, ydx_app_host: string, user_ids: string[]) {
-    const videoData = await MongoVideosModel.findOne({ youtube_id: youtube_id, youtube_status: 'available' });
+    const youtubeVideoData = await getVideoDataByYoutubeId(youtube_id);
+    if (!youtubeVideoData) throw new Error('notifyAiDescriptions :Video not found');
     const captionRequest = await MongoAICaptionRequestModel.findOne({ youtube_id: youtube_id });
     const userIds = captionRequest.caption_requests;
 
     const previewURL = `${ydx_app_host}/audio-description/preview/${youtube_id}/${audio_description_id}`;
-    const subject = `Requested Audio Description for ${videoData.title} ready`;
+    const subject = `Requested Audio Description for ${youtubeVideoData.title} ready`;
     const text = `Your AI prompted description is ready! You're invited to view it by following this link ${previewURL}`;
     for (const user_id of userIds) {
       const email = await getEmailForUser(user_id.toString());
