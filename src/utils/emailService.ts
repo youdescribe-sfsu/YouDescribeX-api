143cb4365e95ba64f3a20233ec123395dc6a1533
sanjay417 <miranisanjay417@gmail.com>
2024-09-15 13:37:51 -0700

    updating emailService.ts for HTML encoded emails

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -1,20 +1,16 @@
-// import nodemailer from 'nodemailer';
 import axios from 'axios';
 import { GMAIL_USER } from '../config';
 import { logger } from './logger';
 
-async function sendEmail(email: string, subject: string, text: string) {
+async function sendEmail(email: string, subject: string, content: string, isHtml = true) {
   try {
     // Configure the email details
     const mailOptions = {
       from: GMAIL_USER, // Sender's email address
       to: email, // Recipient's email address
       subject: subject, // Email subject
-      text: text, // Email text
+      html: content, // Always send as HTML
     };
-
-    // Send the email
-    // const info = await transporter.sendMail(mailOptions);
 
     const config = {
       method: 'post',
@@ -28,7 +24,7 @@ async function sendEmail(email: string, subject: string, text: string) {
 
     const info = await axios.request(config);
     console.log(`Email sent :: ${email}`);
-    logger.info(`Email send :: ${email}`);
+    logger.info(`Email sent :: ${email}`);
   } catch (error) {
     logger.error(`Error Sending Email Due to Error :: ${error} `);
     console.error('Error sending email:', error);
