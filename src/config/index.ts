a5d8209d17df3f57c6015a439bbdc79bd0b9955d
XuanDuyAnhNguyen <duyanh1202@gmail.com>
2024-12-23 15:33:13 -0800

    add api for apple signin

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,7 +9,8 @@ config({ path: `.env.${process.env.NODE_ENV || 'production'}.local` });
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, HOST, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
 export const { YOUTUBE_API_KEY, YOUTUBE_API_URL } = process.env;
-export const { PASSPORT_REDIRECT_URL, PASSPORT_CALLBACK_URL, CRYPTO_SECRET, CRYPTO_SEED } = process.env;
+export const { PASSPORT_REDIRECT_URL, PASSPORT_CALLBACK_URL, CRYPTO_SECRET, CRYPTO_SEED, APPLE_CALLBACK_URL, APPLE_TEAM_ID, APPLE_KEY_ID, APPLE_CLIENT_ID } =
+  process.env;
 export const { MONGO_DB_HOST, MONGO_DB_PORT, MONGO_DB_DATABASE, MONGO_DB_USER, MONGO_DB_PASSWORD } = process.env;
 export const { POSTGRES_DB_NAME, POSTGRES_DB_USER, POSTGRES_DB_PASSWORD, POSTGRES_DB_HOST, POSTGRES_DB_PORT } = process.env;
 export const { CURRENT_DATABASE } = process.env;
