36429226c3e0aaa4be95bbbf8c24ac54b0a2a54a
lanado <do.ng@northeastern.edu>
2026-03-05 15:57:36 -0800

    update allowed doman

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,7 +3,7 @@ import { logger } from '../utils/logger';
 
 class AuthService {
   validateReturnUrl = url => {
-    const allowedDomains = ['ydx.youdescribe.org', 'localhost', process.env.ALLOWED_REDIRECT_DOMAIN];
+    const allowedDomains = ['ydx.youdescribe.org', 'localhost', 'ydx-dev.youdescribe.org', process.env.ALLOWED_REDIRECT_DOMAIN];
     try {
       const parsedUrl = new URL(url);
       return allowedDomains.includes(parsedUrl.hostname);
