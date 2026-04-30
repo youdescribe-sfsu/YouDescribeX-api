bf97403730d32313a5a73adaaea16ef548d462d8
lanado <do.ng@northeastern.edu>
2026-04-30 12:24:17 -0700

    update auth session name so users don't need to clear cookies

--- src/app.ts
+++ src/app.ts
@@ -64,7 +64,7 @@ class App {
     this.app.use(cookieParser());
     this.app.use(
       cookieSession({
-        name: 'auth-session',
+        name: 'auth-session-v2',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         secret: 'YouDescribe Secret',
         domain: process.env.SESSION_COOKIE_DOMAIN,
